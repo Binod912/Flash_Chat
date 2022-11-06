91219543b246ec14c06cf8e021ffa5e5ebf991dc
Binod912 <93367064+Binod912@users.noreply.github.com>
2022-11-06 14:20:56 +0530

    Add files via upload

---
+++ Flash chat/Flash chat/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Flash chat
+//
+//  Created by Binod Sahoo on 06/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        b()
+        // Do any additional setup after loading the view.
+    }
+
+    func b(){
+    print("fuirst")
+    }
+}
+
+var p = "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"
